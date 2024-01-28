d9e346e18ac0c8e3a6cbcfcc96fc8883953776ac
roler <11955735@ieselgrao.org>
2024-01-28 20:51:00 +0100

    ALPHA 12

--- app/src/main/java/com/example/iesgrao/ErasmusAccreditationScreen.kt
+++ app/src/main/java/com/example/iesgrao/ErasmusAccreditationScreen.kt
@@ -1,17 +1,178 @@
 package com.example.iesgrao
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.iesgrao.ui.theme.IESGRAOTheme
 
 @Composable
-fun MyAccreditation(navHostController: NavHostController) {
-    LazyColumn(modifier = Modifier.fillMaxSize()
+fun MyAccreditation(navController: NavHostController) {
+    LazyColumn(modifier = Modifier
+        .fillMaxSize()
         .padding(16.dp)) {
 
+        item { AccreditationContent() }
+        item { ReturnToHome(navController = navController) }
+        item { Footer() }
+
+    }
+}
+
+@Composable
+fun AccreditationContent() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.obj1),
+            contentDescription = null,
+            modifier = Modifier
+                .size(130.dp)
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .align(Alignment.CenterHorizontally)
+        )
+
+        Objectives()
+
+        Card {
+            Column(modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()) {
+                Text(
+                    text = "Mejorar la competencia plurilingüe y la competencia comunicativa en lengua extranjera del alumnado y del profesorado.\n" +
+                            "\nAumentar la competencia digital del profesorado según el nuevo marco común de la competencia digital docente para impulsar y consolidar el uso de metodologías y herramientas digitales.\n" +
+                            "\nFavorecer la inclusión y reducir la conflictividad, atendiendo a la diversidad sociocultural del alumnado y poniendo de relieve la importancia de la interculturalidad.\n" +
+                            "\nFortalecer las relaciones del centro con el tejido comunitario para favorecer una ciudadanía responsable y aumentar el impacto del aprendizaje del alumnado en el desarrollo de sus competencias.",
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+            }
+
+        }
+        Image(
+            painter = painterResource(id = R.drawable.obj2),
+            contentDescription = null,
+            modifier = Modifier
+                .size(130.dp)
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .align(Alignment.CenterHorizontally))
+
+        Objectives()
+
+        Card {
+            Column(modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()) {
+                Text(
+                    text = "Mejorar la competencia plurilingüe y la competencia comunicativa en lengua extranjera del alumnado y del profesorado.\n" +
+                            "\nAumentar la competencia digital del profesorado según el nuevo marco común de la competencia digital docente para impulsar y consolidar el uso de metodologías y herramientas digitales.\n" +
+                            "\nMejorar la empleabilidad del alumnado fomentando el pleno desarrollo de sus capacidades y favoreciendo la adquisición de las competencias y «soft skills» que faciliten su inserción en el mundo laboral.\n" +
+                            "\nFortalecer las relaciones del centro con el tejido comunitario para favorecer una ciudadanía responsable y aumentar el impacto del aprendizaje del alumnado en el desarrollo de sus competencias.",
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+            }
+
+        }
+
+    }
+
+}
+
+@Composable
+fun ReturnToHome(navController: NavController) {
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxSize()) {
+        FloatingActionButton(
+            onClick = {
+                navController.navigate(NavigationActivity.HomeScreenStudent.route)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            content = {
+                Text(text = "Volver al Home",
+                    fontWeight = FontWeight.Bold)
+            }
+        )
+    }
+}
+
+@Composable
+fun Footer() {
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxSize()) {
+
+        Text(
+            text = "Los proyectos KA120-SCH y KA120-VET están cofinanciados por el programa Erasmus+ de la Unión Europea. El contenido de la web es responsabilidad exclusiva del IES El Grao y ni la Comisión Europea, ni el Servicio Español para la Internacionalización de la Educación (SEPIE) son responsables del uso que pueda hacerse de la información aquí difundida.\n",
+            modifier = Modifier.background(MaterialTheme.colorScheme.background.copy(alpha = 0.1f)),
+            fontStyle = FontStyle.Italic,
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center)
+
+        Image(
+            painter = painterResource(id = R.drawable.europe),
+            contentDescription = null,
+            modifier = Modifier
+                .size(130.dp)
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .align(Alignment.CenterHorizontally))
+    }
+}
+
+@Composable
+fun Objectives() {
+    Divider(modifier = Modifier.padding(2.dp))
+
+    Text(
+        text = "Objetivos",
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp),
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        color = MaterialTheme.colorScheme.primary,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ErasmusAccreditationsPreview() {
+    IESGRAOTheme {
+        ConstraintLayout {
+            val navController = rememberNavController()
+            MyAccreditation(navController = navController)
+        }
     }
 }
--- app/src/main/java/com/example/iesgrao/ErasmusScreen.kt
+++ app/src/main/java/com/example/iesgrao/ErasmusScreen.kt
@@ -2,14 +2,18 @@ package com.example.iesgrao
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
@@ -19,10 +23,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,6 +54,13 @@ fun MyErasmus(navController: NavHostController) {
             Spacer(modifier = Modifier.height(16.dp))
             SectionTitle(text = "Proyectos y Acreditaciones")
             MoreOptions(navController)
+        }
+
+        item {
+
+
+
+
         }
     }
 }
@@ -138,6 +149,13 @@ fun MoreOptions(navController: NavController) {
                     fontWeight = FontWeight.Bold)
             }
         )
+        Image(
+            painter = painterResource(id = R.drawable.erasmus),
+            contentDescription = null,
+            modifier = Modifier
+                .size(155.dp)
+                .fillMaxWidth()
+                .fillMaxHeight().align(Alignment.CenterHorizontally))
     }
 }
 
