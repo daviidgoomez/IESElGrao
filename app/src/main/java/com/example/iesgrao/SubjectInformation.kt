1b263e3ab087b970a5dd9b46025c7066a4f9e310
davgomkor <11955735@ieselgrao.org>
2024-01-31 12:24:31 +0100

    Primera versión funcional

--- app/src/main/java/com/example/iesgrao/SubjectInformation.kt
+++ app/src/main/java/com/example/iesgrao/SubjectInformation.kt
@@ -1,25 +1,28 @@
 package com.example.iesgrao
 
+import androidx.annotation.DrawableRes
+
 data class SubjectInformation(
     val id_subject: String,
     val nombre: String,
     val num_horas: Int,
     val descripcion: String?,
-    val curso : CourseInformation
+    val curso : CourseInformation,
+    @DrawableRes val picture: Int
 )
 val subjects = mutableListOf(
     SubjectInformation("subject001","Acceso a Datos",230,
         "Acceso a ficheros, bases de datos relacionales (restricciones), MongoDB (DB noSQL)" +
-                "y Proyecto Iberdrola", courses[1]),
+                "y Proyecto Iberdrola", courses[1], R.drawable.acceso_a_datos),
 
     SubjectInformation("subject002","Programación de Aplicaciones Multimedia",200,
         "Lenguaje de programación Kotlin, desarrollo de Apps en Kotlin con XML y Jetpack Compose y Unity"
-    , courses[1]),
+    , courses[1], R.drawable.jetpack_compose),
     SubjectInformation("subject003","Bases de Datos",1000,"Fundamentos" +
-            "de las Bases de Datos relacionales" , courses[1]),
+            "de las Bases de Datos relacionales" , courses[1], R.drawable.base_de_datos),
     SubjectInformation("subject003","Sistemas de Gestión Empresarial",200,
         "Conocimiento sobre lo que es un ERP, aprendiazaje de python y desarollo de módulos de" +
-                "Odoo", courses[1]),
+                "Odoo", courses[1], R.drawable.odoo),
     SubjectInformation("subject004","Programación de Servicios y Procesos",160,
-        "Lanzamiento y ejecución de hilos en java", courses[1])
+        "Lanzamiento y ejecución de hilos en java", courses[1], R.drawable.psp)
 )
--- app/src/main/java/com/example/iesgrao/SubjectsScreen.kt
+++ app/src/main/java/com/example/iesgrao/SubjectsScreen.kt
@@ -2,17 +2,34 @@ package com.example.iesgrao
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,20 +49,30 @@ fun MySubjects(navController: NavHostController) {
         .fillMaxSize()) {
 
         item { SubjectTitle() }
-        item { SubjectContent() }
+        items(subjects) { subject ->
+            SubjectContent(subject = subject)
+        }
+        item { SubjectHome(navController = navController) }
     }
 }
+
 @Composable
 fun SubjectTitle() {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Text(text = "Mis Asignaturas",modifier = Modifier
+    Row(
+        modifier = Modifier
             .fillMaxWidth()
-            .padding(bottom = 8.dp),
+            .padding(bottom = 8.dp)
+    ) {
+        Text(
+            text = "Mis Asignaturas",
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
             fontWeight = FontWeight.Bold,
             fontSize = 20.sp,
             color = MaterialTheme.colorScheme.primary,
-            textAlign = TextAlign.Center)
-
+            textAlign = TextAlign.Center
+        )
     }
     Divider(modifier = Modifier
         .fillMaxWidth()
@@ -53,9 +80,68 @@ fun SubjectTitle() {
 }
 
 @Composable
-fun SubjectContent() {
-    Column(modifier = Modifier.padding(16.dp).fillMaxSize()) {
+fun SubjectContent(subject: SubjectInformation) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { }
+            .clip(RoundedCornerShape(8.dp)),
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = subject.picture),
+                contentDescription = "Subject Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = subject.nombre,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "Horas: ${subject.num_horas}",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold
 
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = subject.descripcion ?: "",
+                textAlign = TextAlign.Center,
+                fontSize = 14.sp,
+
+            )
+        }
+    }
+}
+
+@Composable
+fun SubjectHome(navController: NavHostController) {
+    Button(
+        onClick = {
+            navController.navigate(NavigationActivity.HomeScreenStudent.route)
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+    ) {
+        Text(
+            text = "Ir al Home",
+        )
     }
 }
 
